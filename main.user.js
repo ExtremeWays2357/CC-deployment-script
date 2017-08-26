72e032efb7a5306476ffa2dc9d01a940b72d2e16
Noodle Skadoodle <catatlive@hotmail.com>
2017-08-26 22:33:26 +0200

    Broke

--- main.user.js
+++ main.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Deployment counter
 // @namespace    http://tampermonkey.net/
-// @version      1.0.1
+// @version      1.0.2
 // @description  Deployment counter
 // @author       Extreme Ways
 // @updateURL    https://github.com/NoodleSkadoodle/CC-deployment-script/raw/master/main.js
@@ -46,37 +46,43 @@
     function deriveDeployment(log){
         logs = log.split('received');
         units = logs[1].split('troops')[0];
-		if (units < 0){
-			console.log("IT WORKS " +units);
-		}
-        addUnitsToPlayer(logs[0], units);
+        addUnitsToPlayer(logs[0], units, 0);
     }
 	function deriveSet(log){
 		logs = log.split('worth');
 		units = logs[1].split('troops')[0];
-        addUnitsToPlayer(logs[0], units);
+        addUnitsToPlayer(logs[0], units, 1);
 	}
     function deriveAutodeploy(log){
         logs = log.split('got bonus of');
         units = logs[1].split('troops')[0];
-        addUnitsToPlayer(logs[0], units);
+        addUnitsToPlayer(logs[0], units, 1);
     }
-    function addUnitsToPlayer(player, units){
+    function addUnitsToPlayer(player, units, deploy){
         player = player.split('"player').pop().split('">')[0];
-        playerTotals[player-1] = parseInt(playerTotals[player-1]) + parseInt(units);
-    }
+		console.log(units);
+		console.log(deploy);
+		console.log(parseInt(units));
+		console.log(playerTotals);
+        playerTotals[player-1][deploy] = parseInt(playerTotals[player-1][deploy]) + parseInt(units);
+		console.log(playerTotals);
+	}
     function printIndividual(){
         for (i = 0; i < playerTotals.length; i++){
-            console.log(playerNames[i] +" has deployed " + playerTotals[i]+" troops.");
-        }
+            console.log(playerNames[i] +" has deployed " + playerTotals[i][0]+" troops.");
+            console.log(playerNames[i] +" has received " + playerTotals[i][1]+" troops as autodeploys.");
+			}
     }
     function printTeam(){
         for (i = 0; i < nrPlayers; i = i+kindOfGame){
-            units = 0;
+            deploys = 0;
+			autodeploys = 0;
             for (j = i; j < i + kindOfGame; j++){
-                units += parseInt(playerTotals[j]);
+                deploys += parseInt(playerTotals[j][0]);
+				autodeploys += parseInt(playerTotals[j][1]);
             }
-            console.log("Team " +(j)/kindOfGame+" has deployed " + units + " troops.");
+            console.log("Team " +(j)/kindOfGame+" has deployed " + deploys + " troops.");
+            console.log("Team " +(j)/kindOfGame+" has received " + autodeploys + " troops as autodeploys.");
         }
     }
 
@@ -117,9 +123,11 @@
     function populateTeams(){
         info = document.getElementById('statsTable');
         var players = info.getElementsByClassName('player');
+		playerTotals = [];
         for (i=0; i < players.length; i++){
             playerNames.push(players[i].innerHTML);
-            playerTotals.push(0);
+			playerTotals.push([]);
+			playerTotals[i].push(0).push(0);
         }
         console.log(playerNames);
         console.log(playerTotals);
