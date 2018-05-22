806d433e1cbd1450d6846e6a978a65646a3f467f
Noël <catatlive@hotmail.com>
2018-05-22 16:49:55 +0200

    format changes
    updated tab vs spaces

--- main.user.js
+++ main.user.js
@@ -10,108 +10,107 @@
 // ==/UserScript==
 
 (function() {
-    var nrPlayers;
-    var kindOfGame;
-    var nrTeams;
-    var playerNames = [];
-    var playerTotals = [];
+	var nrPlayers;
+	var kindOfGame;
+	var nrTeams;
+	var playerNames = [];
+	var playerTotals = [];
 
-    function executeCounting(){
+	function executeCounting(){
 		populateTeams();
-        rawlog = document.getElementById('log').innerHTML;
-        logarray = rawlog.split('<br>');
-        handleLog(logarray);
-        printIndividual();
-        if (kindOfGame != 1)
-            printTeam();
-    }
-    function handleLog(log){
+		const rawlog = document.getElementById('log').innerHTML;
+		const logarray = rawlog.split('<br>');
+		handleLog(logarray);
+		printIndividual();
+		if (kindOfGame != 1)
+			printTeam();
+	}
+	function handleLog(log){
 		/**
-		Log: the omplete game log, split into an array
+		Log: the complete game log, split into an array
 		Reads every line of the log to see if it's about receiving troops. Listens to 'received', 'played a set' and 'troops added to'.
 		*/
-        for (i=0; i < log.length; i++){
-            log[i] = log[i].split(':').pop();
-            if (log[i].includes('received')){
-                deriveDeployment(log[i]);
-            }
-			if (log[i].includes('played a set'))
-				try{
-					deriveSet(log[i]);
-				} catch(err){
-					//Set is nuclear or zombie.
-				}
-            else if (log[i].includes('troops added to'))
-                deriveAutodeploy(log[i]);
-            else{
-                log.splice(i,1);
-                i--;
-            }
-        }
-    }
-    function deriveDeployment(log){
-        /**
+		for (i=0; i < log.length; i++){
+		    let log[i] = log[i].split(':').pop();
+		    if (log[i].includes('received')){
+			deriveDeployment(log[i]);
+		    }
+				if (log[i].includes('played a set'))
+					try{
+						deriveSet(log[i]);
+					} catch(err){
+						//Set is nuclear or zombie.
+					}
+		    else if (log[i].includes('troops added to'))
+			deriveAutodeploy(log[i]);
+		    else{
+			log.splice(i,1);
+			i--;
+		    }
+		}
+	}
+	function deriveDeployment(log){
+		/**
 		log: the log entry
 		Derives the amount of deploy received.
 		*/
-		logs = log.split('received');
-        units = logs[1].split('troops')[0];
-        addUnitsToPlayer(logs[0], units, 0);
-    }
+		let logs = log.split('received');
+		let units = logs[1].split('troops')[0];
+		addUnitsToPlayer(logs[0], units, 0);
+	}
 	function deriveSet(log){
 		/**
 		log: the log entry
 		Derives the amount of deploy from a set received.
 		*/
-		logs = log.split('worth');
-		units = logs[1].split('troops')[0];
-        addUnitsToPlayer(logs[0], units, 0);
+		let logs = log.split('worth');
+		let units = logs[1].split('troops')[0];
+		addUnitsToPlayer(logs[0], units, 0);
 	}
-    function deriveAutodeploy(log){
+    	function deriveAutodeploy(log){
 		/**
 		log: the log entry
 		Derives the amount of autodeploy received.
 		*/
-        logs = log.split('got bonus of');
-        units = logs[1].split('troops')[0];
-        addUnitsToPlayer(logs[0], units, 1);
-    }
-    function addUnitsToPlayer(player, units, deploy){
-        /**
+		logs = log.split('got bonus of');
+		units = logs[1].split('troops')[0];
+		addUnitsToPlayer(logs[0], units, 1);
+    	}
+    	function addUnitsToPlayer(player, units, deploy){
+		/**
 		player: Part of the log entry with the player number.
 		inits: the amount of troops received
 		deploy: 0 for deploy, 1 for autodeploy.
 		*/
 		player = player.split('"player').pop().split('">')[0];
-        playerTotals[player-1][deploy] = parseInt(playerTotals[player-1][deploy]) + parseInt(units);
-
+        	playerTotals[player-1][deploy] = parseInt(playerTotals[player-1][deploy]) + parseInt(units);
 	}
-    function printIndividual(){
+	function printIndividual(){
 		/**
 		Prints the amount of troops deployed and autodeployed to console individually
 		*/
-        for (i = 0; i < playerTotals.length; i++){
-            console.log(playerNames[i] +" has deployed " + playerTotals[i][0]+" troops.");
-            if (playerTotals[i][1] > 0)
-				console.log(playerNames[i] +" has received " + playerTotals[i][1]+" troops as autodeploys.");
+        	for (i = 0; i < playerTotals.length; i++){
+            	console.log(playerNames[i] +" has deployed " + playerTotals[i][0]+" troops.");
+            	if (playerTotals[i][1] > 0)
+			console.log(playerNames[i] +" has received " + playerTotals[i][1]+" troops as autodeploys.");
 		}
-    }
-    function printTeam(){
+	}
+    	function printTeam(){
 		/**
 		Prints the amount of troops deployed and autodeployed to console per team.
 		*/
-        for (i = 0; i < nrPlayers; i = i+kindOfGame){
-            deploys = 0;
-			autodeploys = 0;
-            for (j = i; j < i + kindOfGame; j++){
-                deploys += parseInt(playerTotals[j][0]);
-				autodeploys += parseInt(playerTotals[j][1]);
-            }
-            console.log("Team " +(j)/kindOfGame+" has deployed " + deploys + " troops.");
-            if (autodeploys > 0)
-				console.log("Team " +(j)/kindOfGame+" has received " + autodeploys + " troops as autodeploys.");
-        }
-    }
+	for (i = 0; i < nrPlayers; i = i+kindOfGame){
+		deploys = 0;
+		autodeploys = 0;
+	    	for (j = i; j < i + kindOfGame; j++){
+			deploys += parseInt(playerTotals[j][0]);
+			autodeploys += parseInt(playerTotals[j][1]);
+	    	}
+	    	console.log("Team " +(j)/kindOfGame+" has deployed " + deploys + " troops.");
+	    	if (autodeploys > 0)
+			console.log("Team " +(j)/kindOfGame+" has received " + autodeploys + " troops as autodeploys.");
+		}
+    	}
 
 	function keepButtonAlive(){
 		var interval = setInterval(function(){
@@ -143,24 +142,24 @@
         initializeTeams();
 	});
 
-    function initializeTeams () {
-        switch(kindOfGame){
+	function initializeTeams () {
+        	switch(kindOfGame){
 			case "Doubles":
-                nrTeams = nrPlayers/2;
-                console.log(nrTeams);
-                kindOfGame = 2;
-                break;
-            case "Triples":
-                nrTeams = nrPlayers/3;
-                console.log(nrTeams);
-                kindOfGame = 3;
-                break;
-            case "Quadruples":
-                nrTeams = nrPlayers/4;
-                console.log(nrTeams);
-                kindOfGame = 4;
-                break;
-            default:
+                		nrTeams = nrPlayers/2;
+				//console.log(nrTeams);
+				kindOfGame = 2;
+				break;
+			case "Triples":
+				nrTeams = nrPlayers/3;
+				//console.log(nrTeams);
+				kindOfGame = 3;
+				break;
+			case "Quadruples":
+				nrTeams = nrPlayers/4;
+				//console.log(nrTeams);
+				kindOfGame = 4;
+				break;
+			default:
 				if (kindOfGame.includes("Poly")){
 					kindOfGame = parseInt(kindOfGame.split('(')[1]);
 					nrTeams = 2;
@@ -169,30 +168,30 @@
 				else{
 					nrTeams = nrPlayers;
 					kindOfGame = 1;
-                }
-                console.log(nrTeams);
-                break;
-        }
-        console.log(kindOfGame);
-		console.log(nrTeams);
-    }
-    function populateTeams(){
+				}
+				console.log(nrTeams);
+				break;
+		}
+        	//console.log(kindOfGame);
+		//console.log(nrTeams);
+	}
+	function populateTeams(){
 		/**
 		Initializes the playerNames array with the names of the players, and creates the playerTotals used for keeping track of units received array.
 		*/
-        info = document.getElementById('statsTable');
-        var players = info.getElementsByClassName('player');
+		info = document.getElementById('statsTable');
+		var players = info.getElementsByClassName('player');
 		playerTotals = [];
 		offset = 1;
 		if ($("span[title='Fog of War']").html() == "Fog")
 			offset++;
-        for (i=0; i < players.length - offset; i++){
-            playerNames.push(players[i].innerHTML);
+		for (i=0; i < players.length - offset; i++){
+	    		playerNames.push(players[i].innerHTML);
 			playerTotals.push([]);
 			playerTotals[i][0] = 0;
 			playerTotals[i][1] = 0;
 		}
-        console.log(playerNames);
-        console.log(playerTotals);	
-    }
+		//console.log(playerNames);
+		//console.log(playerTotals);	
+	    }
 })();
